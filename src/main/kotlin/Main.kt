9523e749a186a20c78690917d7cb9e7feeecd2f3
MrKearns <jkearns631@students.mchenry.edu>
2023-01-17 12:09:11 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("Hello my make is Jonathan")
+    println("I live in Crystal Lake")
+    println("I'm working on getting the iOS Development Certificate")
+    println("I went to film school MANY years ago in Seattle, WA")
+    println("I'm hoping these classes at MCC lead to a successful career/life change")
+
+    println("Program arguments: ${args.joinToString()}")
+}
